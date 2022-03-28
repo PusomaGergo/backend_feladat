7dec296e5a246830db1acc5c14234abc26b73e1d
Pusoma Gergő <pusoma.gergo@jedlik.helyi>
2022-03-28 12:08:56 +0200

    kezs :)

--- hahu/models/kategoria.js
+++ hahu/models/kategoria.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const kategoriaSchema = new Schema({
-    _id: Number,
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        maxlength: 30
-    }
+  _id: Number,
+  nev: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: 30,
+    ref: "Hirdetes",
+  },
 });
 
-module.exports = mongoose.model('kategoria', kategoriaSchema, 'kategoriak')
+module.exports = mongoose.model("Kategoria", kategoriaSchema, "kategoriak");
--- hahu/routes/hahu.js
+++ hahu/routes/hahu.js
@@ -1,29 +1,71 @@
-var express = require('express');
-const hirdetes = require('../models/hirdetes');
+const { json } = require("express");
+var express = require("express");
 var router = express.Router();
 
-const Hirdetes = require('../models/hirdetes');
+const Hirdetes = require("../models/hirdetes");
+const kategoria = require("../models/kategoria");
 
-/* GET home page. */
-router.post('/', function(req, res, next) {
+router.post("/", function (req, res, next) {
   const _id = req.body._id;
-  const kategoria = req.body._id;
-  const cim = req.body.kategoria;
+  const kategoria = req.body.kategoria;
+  const cim = req.body.cim;
   const leiras = req.body.leiras;
   const hirdetesDatuma = req.body.hirdetesDatuma;
-  const serulesMentes = req.body.serulesMentes;
+  const serulesmentes = req.body.serulesmentes;
   const arFt = req.body.arFt;
   const kepUrl = req.body.kepUrl;
 
-  const hirdetes = new Hirdetes({_id, kategoria, cim, leiras, hirdetesDatuma, serulesMentes, arFt, kepUrl});
+  const hirdetes = new Hirdetes({
+    _id,
+    kategoria,
+    cim,
+    leiras,
+    hirdetesDatuma,
+    serulesmentes,
+    arFt,
+    kepUrl,
+  });
 
   hirdetes
-  .save()
-  .then(
+    .save(function (err) {
+      if (err) {
+        return res.status(400).json({ success: false, message: err.message });
+      }
+      res.status(200).json({ success: true });
+    })
+    .then(
       res.json({
-      'status' : 'created'
-  }))
-  .catch(err => console.log('Error'))
+        status: "created",
+      })
+    )
+    .catch((err) => console.log(err));
 });
+
+router.get("/:mezo", function (req, res, next) {
+  const mezo = req.params.mezo;
+  Hirdetes.find()
+    .populate("kategoria", "-_id")
+    .sort({ [mezo]: 1 })
+    .then((hirdetesek) => {
+      res.json(hirdetesek);
+    })
+    .catch((err) => console.log(err));
+});
+
+router.delete("/:id", function(req,res, next){
+  const id = req.params.id;
+  Hirdetes.findById(id).then((doc) => {
+    if(doc === null) {
+      return res.json({
+        'error': `A hirdetés ${id} azonosítóval nem létezik!`
+      })
+    }
+    Hirdetes.findByIdAndDelete(id)
+    .then(res.json({ 'status': `A hirdetés ${id} azonosítóval törölve!`}))
+    .catch(err => console.log(err))
+  })
+  
+
+})
 
 module.exports = router;
